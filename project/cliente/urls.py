1c68069abbd4806b87030912a4ff044b8060e23b
Nicolas Barsky <nicobarsky01@gmail.com>
2024-01-31 18:59:50 -0300

    a

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -1,12 +1,21 @@
 
 from django.urls import path
-from .views import index
 from . import views
-
 app_name = "cliente"
+
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("comentarios/list")
-    
+
+    path("clientes/list", views.clientes_list, name="clientes_list"),
+    path("cliente/add", views.clientes_add, name="clientes_add"),
+    path("cliente/clientes", views.Cliente, name="cliente"),
+    path("cliente/publicacion_add", views.publicacion_add, name="publicacion_add"),
+    path("index/list", views.publicacion_list, name="publicaciones_list"),
+    path("cliente/comentarios", views.comentarios, name="comentarios"),
+    path("comentarios/comentarios_listado", views.comentarios_listado, name="comentarios_listado"),
+    path("cliente/comentarios_add", views.comentarios_add, name="comentarios_add"),
+
 ]
+
